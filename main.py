afb11d80e3bbc3094dd7e2d8f1b933aeaebb7b87
Gaoshuaiqi <gsq@qq.com>
2024-08-05 16:04:10 +0800

    添加main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {"msg": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
